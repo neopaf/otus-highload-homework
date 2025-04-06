c8263d1c9254a51a7aa36089bc85e1b7495ef3b7
Петросян Александр Фридрихович <afpetrosyan@beeline.ru>
2025-04-06 10:45:13 +0300

    first/second swapped

---
+++ src/test/java/ru/paf/highload/invoker/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.paf.highload.invoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
